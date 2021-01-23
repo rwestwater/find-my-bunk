bbcf380726288eb28dbc91b556972414831af269
Rachel Westwater <rachel-westwater@hotmail.com>
2021-01-23 15:42:10 +0000

    sorting syntax in main func to allow it to run

--- find_my_bunk.py
+++ find_my_bunk.py
@@ -1,11 +1,15 @@
 from models import scraper
 
-def main():
+scraper = scraper.Scraper
+
+def main(scraper):
     airbnb_urls = [
                 'https://www.airbnb.co.uk/rooms/33571268',
                 'https://www.airbnb.co.uk/rooms/33090114',
                 'https://www.airbnb.co.uk/rooms/40558945'
                 ]
-
+    
+    scraper(airbnb_urls)
+    
 if __name__ == '__main__':
-  main()
+  main(scraper)
