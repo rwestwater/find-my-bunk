a0976a1ecc6513445c30272c80a87d462f3c2951
Rachel Westwater <rachel-westwater@hotmail.com>
2021-01-23 14:57:22 +0000

    renaming the main script

---
+++ find_my_bunk.py
@@ -0,0 +1,11 @@
+from models import scraper
+
+def main():
+    airbnb_urls = [
+                'https://www.airbnb.co.uk/rooms/33571268',
+                'https://www.airbnb.co.uk/rooms/33090114',
+                'https://www.airbnb.co.uk/rooms/40558945'
+                ]
+
+if __name__ == '__main__':
+  main()
