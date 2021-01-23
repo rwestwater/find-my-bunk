a8ac6a4d23dfbb465c5a0babc3c367c6f68fec67
Rachel Westwater <rachel-westwater@hotmail.com>
2021-01-23 13:33:40 +0000

    adding some pseudocode for the methods I want

--- scraper.py
+++ scraper.py
@@ -3,12 +3,14 @@ from requests_html import HTMLSession
 
 class Scraper:
 
+    property_name = ""
     property_type = ""
-    number_of_bedrooms = ""
-    number_of_bathrooms = ""
-    list_all_amenities = ""
+    number_of_bedrooms = int
+    number_of_bathrooms = int
+    list_all_amenities = {}
 
-    def start_session():
+    # initialise the class (constructor)
+    def __init__():
         # initialising html session class
         session = HTMLSession()
         headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
@@ -23,6 +25,15 @@ class Scraper:
 
         property_name = response.html.find('#site-content > div > div > div:nth-child(1) > div:nth-child(1) > div > div > div > div > section > div > div._mbmcsn > h1')
         print(property_name[0].text)
+
+        # def run_through_urls(urls):
+            # for url in urls:
+
+                # def_get_property_name
+                # def_get_property_type
+                # def_get_number_of_bedrooms
+                # get_number_of_bathrooms
+                # get_dict_of_amenities
 
         # for name in property_name:
         #     property = {'property name': name.text}
