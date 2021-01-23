9fe08d153869210eae32158fb6c7333b03331263
Rachel Westwater <rachel-westwater@hotmail.com>
2021-01-23 19:09:57 +0000

    added functionality for list of amenities - backend MVP complete

--- models/scraper.py
+++ models/scraper.py
@@ -30,32 +30,36 @@ class Scraper():
         property_type = all_property_info[property_type_index_position] 
         print("Property type: " + property_type)
 
-    def get_number_of_bedrooms(self, all_property_info):
+    def get_number_of_beds_and_baths(self, all_property_info):
         bedrooms_index_position = [i for i, str in enumerate(all_property_info) if 'guests' in str]
         bedrooms_index_position = int(bedrooms_index_position[0])
         number_of_bedrooms = all_property_info[bedrooms_index_position] 
-        print("Number of bedrooms: " + number_of_bedrooms)
+        number_of_bedrooms = number_of_bedrooms.split("·")
+        print("Number of bedrooms: " + number_of_bedrooms[1])
+        print("Number of bathrooms: " + number_of_bedrooms[3])
+        print("Sleeps: " + number_of_bedrooms[0])
 
-    def get_number_of_bathrooms(self, all_property_info):
-        bathrooms_index_position = [i for i, str in enumerate(all_property_info) if 'guests' in str]
-        bathrooms_index_position = int(bathrooms_index_position[0])
-        number_of_bathrooms = all_property_info[bathrooms_index_position]
-        print("Number of bathrooms: " + number_of_bathrooms) 
+    # def get_number_of_bathrooms(self, all_property_info):
+    #     bathrooms_index_position = [i for i, str in enumerate(all_property_info) if 'guests' in str]
+    #     bathrooms_index_position = int(bathrooms_index_position[0])
+    #     number_of_bathrooms = all_property_info[bathrooms_index_position]
+    #     # print("Number of bathrooms: " + number_of_bathrooms) 
 
     def get_list_of_amenities(self, all_property_info):
-        list_of_amenities = response.html.find('#site-content > div > div._qcb6sd > div:nth-child(2) > div:nth-child(8) > div > div > div > div:nth-child(2) > div._1byskwn', first=True)
-        print(list_of_amenities.text)
+        amenities_index_position = [i for i, str in enumerate(all_property_info) if 'Amenities:' in str]
+        amenities_index_position = int(amenities_index_position[0])
+        list_of_amenities = all_property_info[amenities_index_position] 
+        print("List of " + list_of_amenities)
 
     def run_everything(self, airbnb_urls):
         for url in airbnb_urls:
             response = self.session.get(url)
 
-            # sleep gives us a little time buffer between actions, allows mutliple scroll to catch all info on page 
+            # sleep gives us a little time buffer between actions
             response.html.render(sleep=5, keep_page=True)
             info = self.get_all_info(response)
             self.get_property_name(info)
             self.get_property_type(info)
-            self.get_number_of_bedrooms(info)
-            # self.get_number_of_bathrooms(info)
-            # self.get_list_of_amenities(info)
-
+            self.get_number_of_beds_and_baths(info)
+            self.get_list_of_amenities(info)
+            print("___________________")
